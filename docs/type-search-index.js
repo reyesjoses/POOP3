62aedb3ada2819fc082722b1cb4ce770a8b639fe
reyesjoses <evarey2207@gmail.com>
2025-03-21 01:31:00 -0600

    add javadoc

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"poop3","l":"Alumno"},{"p":"poop3","l":"Pokemon"},{"p":"poop3","l":"POOP3"},{"p":"poop3","l":"Punto"}];updateSearchResults();
